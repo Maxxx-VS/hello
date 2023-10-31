c76c58f106767fb4c50ca1b1ac9dcac51a97ac3a
Высочанский Максим <126272688+maxxx-vs@users.noreply.github.com>
2023-10-31 21:00:04 +0600

    My Django Project #17 Добавлена регистрация пользователя и аутентификация

--- urls.py
+++ urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
-from . import views, admin
+from . import views
+from django.contrib import admin
 from hello.views import secret_page
+
 
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contripb.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('secret_page/', secret_page, name='secret_page'),
 ]
